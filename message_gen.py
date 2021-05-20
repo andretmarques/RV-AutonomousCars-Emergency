5b01866e21df2af3e71c628036b42b2eb6c2fa6b
joaoachando <joaoachando@tecnico.ulisboa.pt>
2021-05-20 10:56:18 +0100

    Add support for stopping Denm

--- message_gen.py
+++ message_gen.py
@@ -1,5 +1,10 @@
 from datetime import datetime
+from time import sleep
+
 from Custom_Class import CAM, DENM, RepeatedTimer
+from Queue import *
+
+from threading import Event, Thread
 
 
 def create_and_send_denm(queue, identifier):
@@ -33,17 +38,28 @@ def cam_loop(queue, identifier, x, y):
 
 def denm_loop(queue, identifier):
     rt = RepeatedTimer(2, create_and_send_denm, queue, identifier)
+    rt.stop()
     return rt
 
 
 def message_generator(queue, event):
+    state = False
     identifier = 9
     cam_timer = cam_loop(queue, identifier, 0, 0)
-
-    event.wait()
-
-    print("Starting DENM")
     denm_timer = denm_loop(queue, identifier)
+
+    while True:
+        while event.is_set():
+            if not state:
+                print("========== Starting DENM ==========")
+                denm_timer.start()
+                state = True
+        if state:
+            print("========== Stopping DENM ==========")
+            denm_timer.stop()
+            state = False
+
+
 
 
 
